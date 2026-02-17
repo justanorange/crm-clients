fc49dfff8f477fef9b1ce6a9b2f68a07b0cb3d1e
Vladimir Lobas <lobasvv@icloud.com>
2026-02-17 19:10:47 +0600

    fix: Production base path changed to repo name

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
+  const base = env.VITE_BASE_PATH || '/';
 
   return {
     plugins: [vue(),tailwindcss(),],
@@ -18,5 +19,6 @@ export default defineConfig(({ mode }) => {
       port: parseInt(env.VITE_PORT),
       strictPort: true,
     },
+    base,
   };
 });
